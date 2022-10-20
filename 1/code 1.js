88268c700e904cdf1a7b8fb5ae328e3ab89dd954
Roma <beyondrom@gmail.com>
2022-10-20 20:35:38 +0300

    Test lesson 1

---
+++ 1/code 1.js
@@ -0,0 +1,22 @@
+
+'use strict';
+
+let persone = {
+    name: "John",
+    age: 21,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+
+let arr = ['plum.png', 'orange.jpeg', 'apple.bmb'];
+
+console.log(arr[1])
+
+alert("hellow world!");
+
+let answer = prompt("Are you sure?", "da");
+console.log(typeof (answer));
+
+
